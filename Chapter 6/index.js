7c532b5a50376b32727fd9ac03f2a0e40a024f04
EduardXanderM <exmmariquita@gmail.com>
2025-03-31 10:58:41 +0400

    update

--- Chapter 6/index.js
+++ Chapter 6/index.js
@@ -1,4 +1,8 @@
-function costCalculate() {
+function costCalculate() { // Calculate the cost of petrol
+    // Get the values from the input fields
+    // If the user does not enter a value, use the default value of 1.72
+    // If the user does not enter a value, use the default value of 0
+    // Calculate the total cost of petrol   
     var price = 0;
     var purchased = 0;
     var total = 0;
